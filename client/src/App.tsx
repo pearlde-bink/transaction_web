e68f78d289e28557b56ccbcba64bc892942a14c0
Bink <tuanbinhnguyenngoc@gmail.com>
2024-08-14 14:31:31 +0700

    add navbar, loader, welcome components

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+// @ts-expect-error //to fix ts type error
+// import { useState } from "react";
+import { Navbar, Footer, Welcome, Services, Transactions } from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
